2e92b56e68ff62b9fc08dec103dd92d1c24c9767
DeeGemini <zodiactheegemini@gmail.com>
2023-09-20 11:02:04 -0700

    updated

--- logic_operator.c
+++ logic_operator.c
@@ -7,29 +7,27 @@
  * @operator_type: The type of logical operator (0 for &&, 1 for ||)
  * Return: 1 on success, 0 on failure.
  */
-int execute_logical_operator(char *left_command, char *right_command,
+int execute_logical_operator(char **left_command, char **right_command,
 	int operator_type)
 {
-	int status;
-
 	if (execute_command(left_command) == 0)
 	{
 		if (operator_type == 0)
 		{
 			if (execute_command(right_command) == 0)
-			return (1);
+				return (1);
 
 			else
-			return (0);
+				return (0);
 		}
 
 		else
 		{
 			if (execute_command(right_command) == 0)
-			return (0);
+				return (0);
 
 			else
-			return (1);
+				return (1);
 		}
 	}
 
